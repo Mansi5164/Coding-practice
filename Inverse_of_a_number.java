1265ab8febd0a98b0f573807e2cb0d86691dc20a
Mansi5164 <156164354+Mansi5164@users.noreply.github.com>
2025-07-05 15:36:44 +0530

    05_July_2025

---
+++ Inverse_of_a_number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Inverse_of_a_number {
+    public static int inverseOfNumber(int n){
+        int ans = 0 , element=1;
+        while(n!=0){
+            int pos = n % 10;
+            ans += element * (int)Math.pow(10,pos-1);
+            n /= 10;
+            element++;
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number : ");
+        int n = sc.nextInt();
+
+        int ans = inverseOfNumber(n);
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
---
+++ rotate_number.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class rotate_number {
+    public static int rotateNumber(int n,int k){
+        int totalDigit = 0,ans=0;
+        int od = n;
+        while(od != 0){
+            od/=10;
+            totalDigit++;
+        }
+        if(k > 0){
+            int lastDig = n % (int)Math.pow(10,k);
+            n = n / (int)Math.pow(10,k);
+            lastDig = lastDig * (int)Math.pow(10,totalDigit-k);
+            ans = lastDig+n;
+        }
+        else{
+            int lastDig = n % (int)Math.pow(10,totalDigit+k);
+            n = n / (int)Math.pow(10,totalDigit+k);
+            lastDig = lastDig * (int)Math.pow(10,k*(-1));
+            ans = lastDig+n;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter number : ");
+        int num = sc.nextInt();
+        System.out.print("enter rotation number : ");
+        int k = sc.nextInt();
+
+        int ans = rotateNumber(num,k);
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
+
+/*
+
+enter number : 562984 
+enter rotation number : -4
+845629
+
+enter number : 562984
+enter rotation number : 2
+845629
+
+ */
